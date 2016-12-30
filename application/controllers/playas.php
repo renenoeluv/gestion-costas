daacb54404366276b2b84bff0e8329e26b1ed831
Pablo Olivares L <pablo.olivares@alumnos.uv.cl>
2016-12-29 22:54:08 -0300

    Grupo 5: Rodrigo Catalán, Pablo Olivares, Aaron Ponce, Sebastian Rubio.

---
+++ application/controllers/playas.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Playas extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('playa_model');
+	}
+
+	public function muestra(){
+		$data['ubicacion']=$this->playa_model->get_ubicacion();
+		$this->load->view('mostrarInfo', $data);
+
+	}
+
+}
