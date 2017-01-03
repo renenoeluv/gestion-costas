473a17424fff3a9209111634559e9bc618ffae8a
root <root@localhost.localdomain>
2017-01-03 12:23:34 -0300

    Vista finalizada, JSON ejemplo agregado en raiz

--- application/controllers/Perfiles.php
+++ application/controllers/Perfiles.php
@@ -64,45 +64,44 @@ class Perfiles extends CI_Controller {
 	return $mediciones;
 	}
 
+
+	//json placeholder segun entrega jueves
+	public function json_tablePH() { 
+		$mediciones = "{
+    columns: [{
+        field: 'dh',
+        title: 'Distancia Horizontal[cm]'
+    }, {
+        field: 'a',
+        title: '24-12-16 Distancia Vertical[m]'
+    }, {
+        field: 'b',
+        title: '25-12-16 Distancia Vertical[m]'
+    }, {
+        field: 'c',
+        title: '26-12-16 Distancia Vertical[m]'
+    }, {
+        field: 'd',
+        title: '27-12-16 Distancia Vertical[m]'
+    }
+    ],
+    data: [{ dh: '0', a: 0, b: 0, c: 0, d: 0 },
+    { dh: '10', a: -0.1,  b: -0.21 , c: -0.3, d: -0.12},
+    { dh: '20', a: -0.15, b: -0.25 , c: -0.4, d: -0.2},
+    { dh: '30', a: -0.17, b: -0.18 , c: -0.35, d: -0.23},
+    { dh: '40', a: -0.17, b: -0.15 , c: -0.37, d: -0.2},
+    { dh: '50', a: -0.19, b: -0.18 , c: -0.36, d: -0.22},
+    { dh: '60', a: -0.18, b: -0.15 , c: -0.39, d: -0.18},
+    { dh: '70', b: -0.18, c: -0.3, d: -0.19},
+    { dh: '80', b: -0.17, d: -0.22},
+    { dh: '90', b: -0.19, d: -0.2}]
+}";
+	return $mediciones;
+	}
+
 	public function index() {
 		$this->load->view('consulta_perfil');
 	}
 
-	/*public function listarAsignaturas() {
-    $this->output
-      ->set_content_type('application/json')
-      ->set_output(json_encode(array('data' => ($this->Asignaturas_model->get_asignaturas() != false > 0 ? $this->Asignaturas_model->get_asignaturas()->result() : ''))));
-			  
-	}
-	public function eliminar() {
-		$cod = $this->input->post('id');
-
-		$this->Asignaturas_model->delete_asignatura($cod);
-		$this->output
-		  ->set_content_type('application/json')
-		  ->set_output(json_encode(array('mensaje' => 'Asignatura eliminada satisfactoriamente.')));
-	}
-
-	public function agregar() {
-		$cod = $this->input->post('codigo');
-		$nom = $this->input->post('nombre');
-		$nalu = $this->input->post('cantidadAumnos');
-
-		$this->Asignaturas_model->set_asignatura($cod, $nom, $nalu);
-    $this->output
-      ->set_content_type('application/json')
-      ->set_output(json_encode(array('mensaje' => 'La asignatura se ha agregado satisfactoriamente.')));
-	}
-
-	public function editar() {
-		$cod = $this->input->post('codigo');
-		$nom = $this->input->post('nombre');
-		$nalu = $this->input->post('cantidadAlumnos');
-
-		$this->Asignaturas_model->update_asignatura($cod, $nom, $nalu);
-		// La asignatura se ha editado satisfactoriamente.
-    $this->output
-      ->set_content_type('application/json')
-      ->set_output(json_encode(array('mensaje' => 'La asignatura se ha editado satisfactoriamente.')));
-	}*/
+	
 }
